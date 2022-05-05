2412dabc99051817e489e88be55ab472a0b5d6bc
manojkumar9908 <manojroyal9908@gmail.com>
2022-05-05 18:01:06 +0530

    add src pom.xml

---
+++ src/main/java/com/sportsshop/SportsshopOrderMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.sportsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsshopOrderMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsshopOrderMsApiApplication.class, args);
+		System.out.println("order ms is placing");
+	}
+
+}
